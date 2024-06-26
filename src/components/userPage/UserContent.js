3063bbe89113f64624b79a9ed32960e52d81b81e
Lisa <lizamotorna2004@gmail.com>
2024-06-26 20:10:21 +0300

    added button switching

--- src/components/userPage/UserContent.js
+++ src/components/userPage/UserContent.js
@@ -1,11 +1,29 @@
 import styles from "./UserContent.module.scss";
 import { UserIcon, CartIcon, FavoritesIcon, ExitIcon } from "../UI/Icons";
 import AuthContext from "../../context/auth-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const UserContent = () => {
+  const [userContent, setUserContent] = useState("");
   const context = useContext(AuthContext);
+
+  const exitHandler = () => {
+    context.logout();
+  };
+
+  const UserContentOptions = () => {
+    switch (userContent) {
+      case "contentOne":
+        return "One";
+      case "contentTwo":
+        return "Two";
+      case "contentThree":
+        return "Three";
+      default:
+        return "One";
+    }
+  };
 
   return (
     <div className={`content ${styles.user}`}>
@@ -14,29 +32,33 @@ const UserContent = () => {
         <div className={styles.user__content}>
           <aside className={styles.user__bar}>
             <button
-              className={`${styles.user__btn} ${styles["user__btn-active"]}`}
+              className={`${styles.user__btn} ${userContent === "contentOne" ? styles["user__btn-active"] : ""}`}
+              onClick={() => setUserContent("contentOne")}
             >
               <UserIcon className={styles.user__icon} />
               Детали профиля
             </button>
-            <button className={styles.user__btn}>
+            <button
+              className={`${styles.user__btn} ${userContent === "contentTwo" ? styles["user__btn-active"] : ""}`}
+              onClick={() => setUserContent("contentTwo")}
+            >
               <CartIcon className={styles.user__icon} />
               Заказы
             </button>
-            <button className={styles.user__btn}>
+            <button
+              className={`${styles.user__btn} ${userContent === "contentThree" ? styles["user__btn-active"] : ""}`}
+              onClick={() => setUserContent("contentThree")}
+            >
               <FavoritesIcon className={styles.user__icon} />
               Список желаемого
             </button>
-            <button
-              className={styles["user__btn-exit"]}
-              onClick={context.logout()}
-            >
+            <button className={styles["user__btn-exit"]} onClick={exitHandler}>
               <NavLink to="/main" replace>
                 <ExitIcon /> Выйти
               </NavLink>
             </button>
           </aside>
-          <div className={styles.user__main}>content</div>
+          <div className={styles.user__main}>{UserContentOptions()}</div>
         </div>
       </div>
     </div>
