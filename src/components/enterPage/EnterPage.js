3076a041df5d6a1d632db4f5df9bbf2befa10635
Lisa <lizamotorna2004@gmail.com>
2024-06-26 18:22:59 +0300

    added exit button and fixed issues on yhe enter page + scss

--- src/components/enterPage/EnterPage.js
+++ src/components/enterPage/EnterPage.js
@@ -9,21 +9,23 @@ const EnterPage = () => {
   return (
     <div className="wrapper">
       <Header />
-      <div className={`container content ${styles.enter__container}`}>
-        <NavLink to="/main" replace>
-          <button className={styles.enter__link}>На главную</button>
-        </NavLink>
-        <div className={styles.enter__content}>
-          <EnterForm />
-          <div className={styles.enter__button}>
-            <span>Нету аккаунта?</span>
-            <NavLink
-              className={styles["enter__button-nav"]}
-              to="/registration"
-              replace
-            >
-              Регистрация
-            </NavLink>
+      <div className={` content ${styles.enter__container}`}>
+        <div className="container">
+          <NavLink to="/main" replace>
+            <button className={styles.enter__link}>На главную</button>
+          </NavLink>
+          <div className={styles.enter__content}>
+            <EnterForm />
+            <div className={styles.enter__button}>
+              <span>Нету аккаунта?</span>
+              <NavLink
+                className={styles["enter__button-nav"]}
+                to="/registration"
+                replace
+              >
+                Регистрация
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
--- src/components/userPage/UserContent.js
+++ src/components/userPage/UserContent.js
@@ -1,33 +1,43 @@
 import styles from "./UserContent.module.scss";
 import { UserIcon, CartIcon, FavoritesIcon, ExitIcon } from "../UI/Icons";
+import AuthContext from "../../context/auth-context";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 
 const UserContent = () => {
+  const context = useContext(AuthContext);
+
   return (
     <div className={`content ${styles.user}`}>
-        <div className="container">
-          <h1 className={styles.user__title}>Личный кабинет</h1>
-          <div className={styles.user__content}>
-            <aside className={styles.user__bar}>
-              <button
-                className={`${styles.user__btn} ${styles["user__btn-active"]}`}
-              >
-                <UserIcon className={styles.user__icon} />
-                Детали профиля
-              </button>
-              <button className={styles.user__btn}>
-                <CartIcon className={styles.user__icon} />
-                Заказы
-              </button>
-              <button className={styles.user__btn}>
-                <FavoritesIcon className={styles.user__icon} />
-                Список желаемого
-              </button>
-              <button className={styles["user__btn-exit"]}>
-                <ExitIcon/> Выйти
-              </button>
-            </aside>
-            <div className={styles.user__main}>content</div>
-          </div>
+      <div className="container">
+        <h1 className={styles.user__title}>Личный кабинет</h1>
+        <div className={styles.user__content}>
+          <aside className={styles.user__bar}>
+            <button
+              className={`${styles.user__btn} ${styles["user__btn-active"]}`}
+            >
+              <UserIcon className={styles.user__icon} />
+              Детали профиля
+            </button>
+            <button className={styles.user__btn}>
+              <CartIcon className={styles.user__icon} />
+              Заказы
+            </button>
+            <button className={styles.user__btn}>
+              <FavoritesIcon className={styles.user__icon} />
+              Список желаемого
+            </button>
+            <button
+              className={styles["user__btn-exit"]}
+              onClick={context.logout()}
+            >
+              <NavLink to="/main" replace>
+                <ExitIcon /> Выйти
+              </NavLink>
+            </button>
+          </aside>
+          <div className={styles.user__main}>content</div>
+        </div>
       </div>
     </div>
   );
