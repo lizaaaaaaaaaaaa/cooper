9030172f83b8f994c87e7c8a15a998d2901a9b88
Lisa <lizamotorna2004@gmail.com>
2024-07-03 22:27:32 +0300

    added deleted avatars and success/failure message

--- src/components/userPage/userPageControl/ProfileDetails.js
+++ src/components/userPage/userPageControl/ProfileDetails.js
@@ -1,61 +1,26 @@
-import React, { useContext } from "react";
-import styles from "../UserContent.module.scss";
+import React, { useContext, useState, useEffect } from "react";
 import AuthContext from "../../../context/auth-context";
-import noUser from "../../../assets/userImage/noUser.jpg";
-import { storage } from "../../../firebase/firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { getDatabase, ref as dbRef, update } from "firebase/database";
+import ProfileGreetings from "./ProfileGreetings";
+import styles from "../UserContent.module.scss";
+import success from "../../../assets/success.svg";
+import failure from "../../../assets/failure.svg";
 
 const ProfileDetails = () => {
   const context = useContext(AuthContext);
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+  const [isMessageShow, setIsMessageShow] = useState(false);
 
-  const username = context.userDetails
-    ? context.userDetails.name || "Неизвестный пользователь"
-    : "Неизвестный пользователь";
-  const avatar = context.userDetails
-    ? context.userDetails.avatar || noUser
-    : noUser;
-
-  const uploadAvatar = async (event) => {
-    const file = event.target.files[0];
-    if (!file) return;
-
-    if (!context.userDetails || !context.userDetails.login) {
-      console.error("User details are not defined or missing email");
-      return;
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        setMessage("");
+        setIsMessageShow(false);
+      }, 5000);
+      return () => clearTimeout(timer);
     }
-
-    try {
-      const storageRef = ref(storage, `userAvatar/${context.userDetails.login}/${file.name}`);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        "state_changed",
-        (error) => {
-          console.error("Error uploading avatar:", error);
-        },
-        async () => {
-          try {
-            // завантаження зображення
-            await uploadTask;
-
-            // Отримання ссилки завантаженої картинки
-            const newAvatarUrl = await getDownloadURL(uploadTask.snapshot.ref);
-            console.log("File available at", newAvatarUrl);
-
-            // Оновлення контексту та локального сховища
-            const updatedUserDetails = { ...context.userDetails, avatar: newAvatarUrl };
-            context.updateUserDetails(updatedUserDetails);
-            localStorage.setItem("userInfo", JSON.stringify(updatedUserDetails));
-          } catch (error) {
-            console.error("Error updating context or local storage:", error);
-          }
-        }
-      );
-    } catch (error) {
-      console.error("Error uploading avatar:", error);
-    }
-  };
+  }, [message]);
 
   const saveUserData = async () => {
     try {
@@ -66,43 +31,43 @@ const ProfileDetails = () => {
 
       const userData = JSON.parse(localStorage.getItem("userInfo"));
       const db = getDatabase(); // Отримати базу даних
-      const userDocRef = dbRef(db, `userEnter/${context.userDetails.key}`); // Створити посилання на документ
-
-      console.log(db);
-      console.log(userDocRef);
+      const userDocRef = dbRef(db, `userEnter/${context.userDetails.key}`); // посилання на документ
 
       await update(userDocRef, {
-        name: userData.name,
         avatar: userData.avatar,
         // Додайте інші поля, які потрібно оновити в базі даних
       });
 
-      console.log("Дані користувача оновлені в базі даних");
+      setMessageType("success");
+      setMessage("Данные сохраненны");
+      setIsMessageShow(true);
     } catch (error) {
-      console.error("Помилка при оновленні даних в базі даних:", error);
+      setMessageType("error");
+      setMessage("Данные не сохраненны.");
+      setIsMessageShow(true);
+      console.error("Помилка при оновленні даних в базі даних: ", error);
     }
   };
 
   return (
     <React.Fragment>
-      <div className={styles.user__profile}>
-        <img src={avatar} className={styles.user__img} alt="user" />
-        <div className={styles.user__greetings}>
-          <p>Добрый день</p>
-          <h4>{username}</h4>
-          <input
-            type="file"
-            accept="image/*"
-            style={{ display: "none" }}
-            id="upload-avatar"
-            onChange={uploadAvatar}
-          />
-          <label htmlFor="upload-avatar" className={styles.change_photo_button}>
-            Сменить фото
-          </label>
-        </div>
-      </div>
+      <ProfileGreetings />
       <button onClick={saveUserData}>Сохранить данные</button>
+      <p
+        className={`${styles.user__message} ${
+          isMessageShow ? styles["user__message-show"] : ""
+        } ${
+          messageType === "success"
+            ? styles["user__message-success"]
+            : styles["user__message-failure"]
+        }`}
+      >
+        <img
+          src={messageType === "success" ? success : failure}
+          alt="messageIcon"
+        />
+        {message}
+      </p>
     </React.Fragment>
   );
 };
---
+++ src/components/userPage/userPageControl/ProfileGreetings.js
@@ -0,0 +1,124 @@
+import styles from "../UserContent.module.scss";
+import React, { useContext } from "react";
+import AuthContext from "../../../context/auth-context";
+import noUser from "../../../assets/userImage/noUser.jpg";
+import { storage } from "../../../firebase/firebase";
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  listAll,
+  deleteObject,
+} from "firebase/storage";
+import { DownloadIcon } from "../../UI/Icons";
+
+const ProfileGreetings = () => {
+  const context = useContext(AuthContext);
+
+  const username = context.userDetails
+    ? context.userDetails.name || "Неизвестный пользователь"
+    : "Неизвестный пользователь";
+  const avatar = context.userDetails
+    ? context.userDetails.avatar || noUser
+    : noUser;
+
+  const deleteOldAvatars = async () => {
+    if (!context.userDetails || !context.userDetails.login) {
+      console.error("Не знайдено логін користувача");
+      return;
+    }
+
+    const userAvatarFolderRef = ref(
+      storage,
+      `userAvatar/${context.userDetails.login}`
+    );
+
+    try {
+      const listResults = await listAll(userAvatarFolderRef);
+      const deletePromises = listResults.items.map((itemRef) =>
+        deleteObject(itemRef)
+      );
+      await Promise.all(deletePromises);
+      console.log("Старі аватари видалені успішно");
+    } catch (error) {
+      console.error("Помилка видалення старих аватарів: ", error);
+    }
+  };
+
+  const uploadAvatar = async (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    if (!context.userDetails || !context.userDetails.login) {
+      console.error("Не знайдено логін користувача.");
+      return;
+    }
+
+    try {
+      // видалення всіх старих аватарів перед завантаженням нового
+      await deleteOldAvatars();
+
+      const storageRef = ref(
+        storage,
+        `userAvatar/${context.userDetails.login}/${file.name}`
+      );
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (error) => {
+          console.error("Помилка завантаження аватара: ", error);
+        },
+        async () => {
+          try {
+            // завантаження зображення
+            await uploadTask;
+
+            // отримання ссилка завантаженої картинки
+            const newAvatarUrl = await getDownloadURL(uploadTask.snapshot.ref);
+            console.log("Місцезнаходженнязавантаженого аватару: ", newAvatarUrl);
+
+            // оновлення контексту та локального сховища
+            const updatedUserDetails = {
+              ...context.userDetails,
+              avatar: newAvatarUrl,
+            };
+            context.updateUserDetails(updatedUserDetails);
+            localStorage.setItem(
+              "userInfo",
+              JSON.stringify(updatedUserDetails)
+            );
+          } catch (error) {
+            console.error("Помилка оновлення контексту або локлаьного сховища: ", error);
+          }
+        }
+      );
+    } catch (error) {
+      console.error("Помилка оновлення аватару: ", error);
+    }
+  };
+
+  return (
+    <div className={styles.user__profile}>
+      <img src={avatar} className={styles.user__img} alt="user" />
+      <div className={styles.user__greetings}>
+        <p>Добрый день,</p>
+        <h4>{username}</h4>
+        <input
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          id="upload-avatar"
+          onChange={uploadAvatar}
+        />
+        <label htmlFor="upload-avatar" className={styles["user__btn-change"]}>
+          <DownloadIcon />
+          Сменить фото
+          <span>(jpeg, png)</span>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileGreetings;
