70bfe6964f2d2984c09be417b916b65583ecdb5f
Lisa <lizamotorna2004@gmail.com>
2024-09-26 16:23:13 +0300

    update fetching data from db in sales and recommendations components

--- src/components/mainSliders/sales/SalesContent.js
+++ src/components/mainSliders/sales/SalesContent.js
@@ -2,56 +2,37 @@ import Slider from "../slider/Slider";
 import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import Loader from "../../UI/Loader";
+import { useContext } from "react";
+import ProductsContext from "../../../context/products-context";
 
 const SalesContent = () => {
   const [sales, setSales] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [httpErrorMessage, setHttpErrorMessage] = useState(false);
+  const context = useContext(ProductsContext);
 
   useEffect(() => {
-    setIsLoading(true);
-    const fetchSales = async () => {
-      try {
-        const response = await fetch(
-          "https://cooper-3c826-default-rtdb.firebaseio.com/catalog.json"
-        );
-        if (!response.ok) {
-          throw new Error("Something went wrong!");
+    const salesArray = [];
+    if (context.products.length > 0) {
+      for (const item of context.products) {
+        if (item.isSale) {
+          salesArray.push(item);
         }
-        const responseData = await response.json();
-
-        const loadedSales = [];
-
-        for (const key in responseData) {
-          const sale = responseData[key];
-          if (sale.isSale === true) {
-            loadedSales.push({
-              id: key,
-              name: responseData[key].name,
-              price: responseData[key].price,
-              image: responseData[key].image,
-              filter: responseData[key].filter,
-              isSale: responseData[key].isSale,
-              salePrice: responseData[key].salePrice,
-            });
-          }
-        }
-        setSales(loadedSales);
-        setIsLoading(false);
-      } catch (error) {
-        setHttpErrorMessage(error.message);
-        setIsLoading(false);
       }
-    };
-    fetchSales();
-  }, []);
+    }
+    setSales(salesArray);
+  }, [context.products]);
 
-  if (isLoading) {
+  if (context.isLoading) {
     return <Loader />;
   }
 
-  if (httpErrorMessage) {
-    return <Navigate to="/httpError" errorMessage={httpErrorMessage} replace />;
+  if (context.httpErrorMessage) {
+    return (
+      <Navigate
+        to="/httpError"
+        errorMessage={context.httpErrorMessage}
+        replace
+      />
+    );
   }
   return (
     <React.Fragment>
--- src/components/mainSliders/weRecommend/WeRecommendContent.js
+++ src/components/mainSliders/weRecommend/WeRecommendContent.js
@@ -2,56 +2,39 @@ import Slider from "../slider/Slider";
 import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import Loader from "../../UI/Loader";
+import { useContext } from "react";
+import ProductsContext from "../../../context/products-context";
 
 const WeRecomendedContent = () => {
   const [recommendations, setRecommendations] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [httpErrorMessage, setHttpErrorMessage] = useState(false);
+  const context = useContext(ProductsContext);
+
+  const filteredKeys = ["c6", "c9", "с11", "c24", "c25", "c29"];
 
   useEffect(() => {
-    setIsLoading(true);
-    const fetchRecommendations = async () => {
-      try {
-        const response = await fetch(
-          "https://cooper-3c826-default-rtdb.firebaseio.com/catalog.json"
-        );
-        if (!response.ok) {
-          throw new Error("Something went wrong!");
+    const recommendationsArray = [];
+    if (context.products.length > 0) {
+      for (const item of context.products) {
+        if (filteredKeys.includes(item.id)) {
+          recommendationsArray.push(item);
         }
-        const responseData = await response.json();
-
-        const loadedRecommendations = [];
-        const filteredKeys = ["c6", "c9", "с11", "c24", "c25", "c29"];
-
-        for (const key in responseData) {
-          if (filteredKeys.includes(key)) {
-            loadedRecommendations.push({
-              id: key,
-              name: responseData[key].name,
-              price: responseData[key].price,
-              image: responseData[key].image,
-              filter: responseData[key].filter,
-              isSale: responseData[key].isSale,
-              salePrice: responseData[key].salePrice,
-            });
-          }
-        }
-        setRecommendations(loadedRecommendations);
-        setIsLoading(false);
-      } catch (error) {
-        setHttpErrorMessage(error.message);
-        setIsLoading(false);
       }
-    };
-    fetchRecommendations();
-  }, []);
+    }
+    setRecommendations(recommendationsArray);
+  }, [context.products]);
 
-  if (isLoading) {
+  if (context.isLoading) {
     return <Loader />;
   }
 
-  if (httpErrorMessage) {
-    return <Navigate to="/httpError" errorMessage={httpErrorMessage} replace />;
+  if (context.httpErrorMessage) {
+    return (
+      <Navigate
+        to="/httpError"
+        errorMessage={context.httpErrorMessage}
+        replace
+      />
+    );
   }
   return (
     <React.Fragment>
